3137a74cc395c92c4b7679632ab86faa2bb39add
Harshit Gajjar <hrstgajjar3@gmail.com>
2023-11-16 10:54:07 -0800

    added result page ui changes

--- client/src/components/result/ResultComponent.jsx
+++ client/src/components/result/ResultComponent.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import award from "../../assets/award.png";
+import failed from "../../assets/failed.png";
+import PrimaryButton from "../../ui/PrimaryButton/PrimaryButton";
+import "./Result.css";
 
 export default function ResultComponent() {
-  return <div>ResultComponent</div>;
+  const currentScore = useSelector((state) => state.quiz.currentScore);
+  const opponentScore = useSelector((state) => state.quiz.opponentScore);
+  return (
+    <div className="result-container">
+      <h2>{`You ${currentScore > opponentScore ? "Win" : "Loose"}`}</h2>
+      {currentScore > opponentScore ? (
+        <img src={award} alt="This is trophy icon" className="award" />
+      ) : (
+        <img src={failed} alt="This is trophy icon" className="award" />
+      )}
+      <label>Don't give up! Better luck next time</label>
+      <div className="result-container-button">
+        <PrimaryButton type="primary">Go to DashBoard</PrimaryButton>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/ResultPage.jsx
+++ client/src/pages/ResultPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ResultComponent from "../components/result/ResultComponent";
+import Header from "../components/header/Header";
 
 export default function ResultPage() {
   return (
-    <div>
+    <>
+      {/* <Header /> */}
       <ResultComponent />
-    </div>
+    </>
   );
 }
